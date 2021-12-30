71f3d3a2c234b0e5016853e91cddf20c06458cf1
MunzurulAzam <munzurul1998@gmail.com>
2021-12-30 17:25:49 +0600

    problem slove

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='curriculum'
-urlpatterns = [
-
-    path('<str:standard>/<slug:slug>/', views.LessonListView.as_view(), name='lesson_list'),
-
-]
